594cdde27a824b50a75700e4568d1eef20c125a4
Kamal Singh <itskamal.dev@gmail.com>
2024-04-06 12:25:25 +0530

    Trying to add google site verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,6 @@ import NextAuthProvider from "@/provider/nextAuthProvider";
 import Navbar from "@/components/Navbar/navbar";
 import Footer from "@/components/footer";
 import Script from "next/script";
-import Head from "next/head";
-
 
 
 
@@ -15,8 +13,21 @@ import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  robots: { index: false, follow: false },
   title: "PlaceForm- Your Ultimate Form and Quiz Creator!",
-  description: "Elevate your form creation game with placeForm – the platform that goes beyond expectations. Whether you're crafting engaging quizzes or creating versatile forms, we've got you covered. Unleash your creativity, gather valuable insights, and streamline data collection with our intuitive form builder.",
+  description: "Elevate your form creation game with placeForm – the platform that goes beyond expectations. ",
+  other: {
+    "google-site-verification": "1z_0Kzwtz-_yVauIBmnCbNouK8QUZvm_A14JDoyGWoI",
+  },
+  verification: {
+    google: "kamal",
+  },
+  openGraph: {
+    title: "PlaceForm- Your Ultimate Form and Quiz Creator!",
+    description: "Elevate your form creation game with placeForm – the platform that goes beyond expectations. ",
+    type: "website",
+    url: "https://placeform.vercel.app",
+  },
 };
 
 export default function RootLayout({
@@ -26,12 +37,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <meta name="google-site-verification" content="1z_0Kzwtz-_yVauIBmnCbNouK8QUZvm_A14JDoyGWoI" />
-      </Head>
+
       <body className={inter.className}>
       <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3285538400653886"/>
         <NextAuthProvider> <Navbar/> {children} <Footer/></NextAuthProvider>
+
       </body>
     </html>
   );
