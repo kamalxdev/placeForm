9be151bdc77673493fa63c7d75c1759708eb003a
Kamal Singh <itskamal.dev@gmail.com>
2024-10-20 14:59:07 +0530

    removed /api path from middleware configuration

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware"
 
 
-export const config = { matcher: ["/dashboard/:path*","/account/:path*","/form/:path*","/quiz/:path*","/api/:path"] }
+export const config = { matcher: ["/dashboard/:path*","/account/:path*","/form/:path*","/quiz/:path*"] }
 
